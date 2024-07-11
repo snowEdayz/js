361c2a29a0f4f68eeafc6335a2e636123f1a2089
snowEdayz <126792672+snowEdayz@users.noreply.github.com>
2024-07-11 19:41:37 +0800

    Update bl.meta.js

--- bilibili/bl.meta.js
+++ bilibili/bl.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Make BiliBili Great Again
 // @namespace    https://www.kookxiang.com/
-// @version      1.5.4
+// @version      1.5.5
 // @description  useful tweaks for bilibili.com
 // @author       kookxiang
 // @match        https://*.bilibili.com/*
